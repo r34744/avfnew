5726627b890a24c32f2f6c5e9f784b2ade441a0d
Greg Koenig <gkoenig@fullsail.edu>
2013-08-24 23:46:10 -0400

    4 in ios, 2 in android

--- platforms/android/bin/AVFNEW-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/bin/AVFNEW-debug-unaligned.apk : \
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/bin/AVFNEW.ap_ \
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/bin/classes.dex \
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/libs/cordova-3.0.0.jar \
--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/bin/AndroidManifest.xml : \
-/Users/Greg/Full Sail/AVF/apps/avfnew/platforms/android/AndroidManifest.xml \
